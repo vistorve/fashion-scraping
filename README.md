447294a475b91afe6ad76125c961eeac7ec36786
Alex Yuskauskas <alex@knock.com>
2018-09-15 21:29:15 -0700

    start

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # fashion-scraping
+
+Scrape fashion blogs and store results
+
+# Installing
+
+`python3 -m venv ~/venvs/fasion-scraping`
+`source ~/venvs/fashion-scraping/bin/activate`
+`pip install -r requirements.txt`
+
+# Running
+
+Run scraper for extra-petite, output to a file. One json per line
+
+`scrapy crawl extra_petite -o extra_petite.jl`
---
+++ fashion/spiders/extrapetite.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class ExtraPetiteSpider(scrapy.Spider):
+    name = 'extra_petite'
+    allowed_domains = ['extrapetite.com']
+    start_urls = ['https://www.extrapetite.com/']
+
+    def parse(self, response):
+        for post in response.css('div.outfit-box'):
+            links = post.css('a::attr(href)').extract()
+            text = post.css('a::text').extract()
+            for l,t in zip(links, text):
+                yield {
+                    'link': l,
+                    'item': t
+                }
+
+        next = response.css('div.nav-pagination').css('div.rvlv_alignright').css('a::attr(href)').extract()[0]
+        yield scrapy.Request(next, callback=self.parse)
